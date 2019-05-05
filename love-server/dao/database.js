630aa2db9f0a0328eba85de5aac08d7e9cd27109
caomiao <648968517@qq.com>
2019-05-05 17:17:44 +0800

    提交了初始化文件

---
+++ love-server/dao/database.js
@@ -0,0 +1,7 @@
+//连接数据库
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://localhost/F56';  // 其中 F56 为连接的数据库名称，可以改变
+mongoose.connect(dbURI, { useNewUrlParser: true });
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
